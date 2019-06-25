99b26320bc5dc7a35641709fec9974273a173669
Michael Emmi <michael.emmi@gmail.com>
2019-06-25 17:52:37 -0400

    Adjusted sexpr interface.

--- src/test/sexpr.ts
+++ src/test/sexpr.ts
@@ -1,4 +1,4 @@
-import { parse } from '../frontend/sexpr';
+import { Expr } from '../frontend/sexpr';
 import assert from 'assert';
 
 const pragmas = `pragma solidity ^0.5.0;`;
@@ -6,12 +6,12 @@ const pragmas = `pragma solidity ^0.5.0;`;
 describe('sexpr', function() {
 
     it('parses simple sexpressions', async function() {
-        const expr = parse("(add a b)");
+        const expr = Expr.parse("(add a b)");
         assert.deepEqual(expr, ["add", "a", "b"]);
     });
 
     it('parses nested sexpressions', async function() {
-        const expr = parse("(add (mult (a (minus c))) b)");
+        const expr = Expr.parse("(add (mult (a (minus c))) b)");
         assert.deepEqual(expr, ["add", ["mult", ["a", ["minus", "c"]]], "b"]);
     });
 
