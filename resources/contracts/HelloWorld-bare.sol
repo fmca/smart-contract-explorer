2d12a7ee2b473953a20d1529dce7159337c4729c
Michael Emmi <michael.emmi@gmail.com>
2019-07-31 15:52:41 -0400

    Refined verifier tests.

--- resources/contracts/HelloWorld-bare.sol
+++ resources/contracts/HelloWorld-bare.sol
@@ -2,7 +2,6 @@ pragma solidity ^0.5.0;
 
 /**
  * @notice simulation HelloWorld.total == HelloAfrica.counter
- * @notice simulation HelloWorld.counter == HelloAfrica.counter
  */
 contract HelloWorld {
 	int total;
